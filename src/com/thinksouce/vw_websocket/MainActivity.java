e46ecc5c069c3a1bbedebbac2e9dc3e35bbfe33d
thinksource <foretribe@gmail.com>
2013-08-02 21:10:16 +1000

    html java edit

--- src/com/thinksouce/vw_websocket/MainActivity.java
+++ src/com/thinksouce/vw_websocket/MainActivity.java
@@ -1,16 +1,36 @@
 package com.thinksouce.vw_websocket;
 
+import com.strumsoft.websocket.phonegap.WebSocketFactory;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);        
+        WebView wv = (WebView)findViewById(R.id.webview);				
+		wv.getSettings().setJavaScriptEnabled(true);
+		wv.setWebChromeClient(new WebChromeClient() {
+			   public void onProgressChanged(WebView view, int progress) {				   
+			   }
+			 });
+		wv.setWebViewClient(
+				new WebViewClient(){
+					public boolean shouldOverrideUrlLoading(WebView view, String url){
+						view.loadUrl(url);
+						return false;
+					}
+		});
+		wv.loadUrl("file:///android_asset/www/index.html");
+		wv.addJavascriptInterface(new WebSocketFactory(wv), "WebSocketFactory");
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
